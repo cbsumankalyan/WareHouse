e2e1cce7415b388f360ed39b3cb0c5c5dfe06ea3
sumancb <sumancb@UNI-BGL-SUMAN.corp.unicity.net>
2021-03-16 14:05:45 +0530

    Test Commit

--- WH/src/main/java/Tests/KYC.java
+++ WH/src/main/java/Tests/KYC.java
@@ -3,10 +3,12 @@ package Tests;
 import org.testng.annotations.Test;
 import Pages.KycPage;
 import Pages.KycSendSMS;
+import Pages.LoginPage;
 import Pages.SuperTestNG;
 
 public class KYC extends SuperTestNG {
 	
+	LoginPage LP = new LoginPage();
 	KycPage kyc = new KycPage();
 	KycSendSMS kycsendsms = new KycSendSMS();
 	
@@ -14,6 +16,7 @@ public class KYC extends SuperTestNG {
 	public void KYCPortal () {
 		
 		test=WarehouseOrders.createNode("<b><font color=407899>"+"KYC Portal" + "</font></b>");
+		LP.login();
 		kyc.KYC();
 		kycsendsms.sendsms();
 	}
